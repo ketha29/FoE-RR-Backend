704c7614bf71389264166d2f8e5034a19c8b2f74
Ketharagan <e20199@eng.pdn.ac.lk>
2024-07-05 06:20:03 +0530

    Initilize a new maven project

---
+++ src/main/java/com/ketha/FoE_RoomReservation/FoERoomReservationApplication.java
@@ -0,0 +1,13 @@
+package com.ketha.FoE_RoomReservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoERoomReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoERoomReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ketha/FoE_RoomReservation/FoERoomReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ketha.FoE_RoomReservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoERoomReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
